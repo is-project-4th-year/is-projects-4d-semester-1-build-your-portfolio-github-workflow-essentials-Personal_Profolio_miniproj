3625fce9be488c14f10d3e49887b463f98391a4c
Mvngii <victor.mbugu@strathmore.edu>
2025-06-28 21:44:10 +0300

    feat: Added style to the about me section.
    Using CSS to enhance the appearance of the about me section with an image and improved layout.
    
    issue: #6

---
+++ script.js
@@ -0,0 +1,17 @@
+console.log("Landing page loaded.");
+
+
+// Fade-in animation when about section comes into view
+/*const aboutSection = document.querySelector('.about');
+
+const observer = new IntersectionObserver(entries => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      aboutSection.classList.add('fade-in');
+    }
+  });
+}, {
+  threshold: 0.2,
+});
+
+observer.observe(aboutSection);*/
